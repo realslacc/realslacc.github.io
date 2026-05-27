bc2ee0f946439ced577e97a2e189b4cb1d9d3ee7
slackwaree <54006754+slackwaree@users.noreply.github.com>
2026-05-27 15:17:53 +1200

    Test commit

---
+++ src/js/blog.js
@@ -0,0 +1,53 @@
+import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js';
+
+let postsArray = [];
+let postNum = 1;
+window.addEventListener('load', loadPost(postNum++));
+
+async function loadPost(postNum) { // posts are named in ascending numerical order
+    const url = `https://d20ryw3o58mey6.cloudfront.net/posts/${postNum}.md`;
+    const res = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+    });
+    if (!res.ok) { // Final blog post reached
+        postsArray.forEach((e) => {
+            renderToHTML(e);
+        });
+        lastPostReached();
+        console.warn('No more posts avaliable!');
+    }
+    else {
+        const getMD = await res.text();
+        postsArray.push(marked.parse(getMD));
+        postNum++;
+        loadPost(postNum);
+    }
+}
+
+const blogContainer = document.querySelector('.blog-container');
+function renderToHTML(e) {
+    const createArticle = document.createElement('article');
+    const renderToHTML = marked.parse(e);
+
+    createArticle.innerHTML = renderToHTML; // Security headers are active to prevent XSS
+    blogContainer.insertBefore(createArticle, blogContainer.firstChild);
+
+    if (postNum = 1) {
+        removeHoldState()
+    }
+}
+
+function removeHoldState() {
+    const holdStatusText = document.querySelector('#hold-status');
+    holdStatusText.textContent = null;
+}
+
+function lastPostReached() {
+    const postCount = localStorage.getItem('lastPost');
+    postsArray.push('green')
+    if (postCount == null || postCount != postsArray.length) {
+        localStorage.setItem('lastPost', postsArray.length);
+        blogContainer.firstChild.classList.add('newPostAlert')
+    }
+}
