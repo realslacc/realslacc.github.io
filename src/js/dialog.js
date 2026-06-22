c7554406cf5da765e937063e2b0504d7296a13f5
slack <54006754+realslacc@users.noreply.github.com>
2026-06-22 15:31:06 +1200

    Update dialog.js

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -4,11 +4,11 @@ localStorage.setItem('enable-updates', null);
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
         if (btn.value == 'enable-updates') {
-            localStorage.setItem('enable-updates', true);
+            localStorage.setItem('enable-updates', 'true');
             localStorage.setItem('current-update', 0);
         }
         else {
-            localStorage.setItem('enable-updates', false);
+            localStorage.setItem('enable-updates', 'false');
         }
         window.location.href ='https://realslacc.github.io';
     });
