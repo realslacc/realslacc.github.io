6d45d2bcee03db2221aab0aac35ef4a467e58ba3
slack <54006754+realslacc@users.noreply.github.com>
2026-06-22 15:28:35 +1200

    Fix URL redirection for updates in blog.js
    Updated URL redirection logic based on localStorage values.

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,14 +1,16 @@
 import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js';
 
 window.addEventListener('load', () => {
-    if (localStorage.getItem('enable-updates') == true) {
+    if (localStorage.getItem('enable-updates') == true || ) {
         if (localStorage.getItem('current-update') != 0) { // hardcoded
             window.location.href = 'https://realslacc.github.io/dialog.html'
         }
     }
     else if (localStorage.getItem('enable-updates') == 'null') {
-        window.location.href = './dialog.html'
+        window.location.href = 'https://realslacc.github.io/dialog.html'
     }
+    else {
+        return;
 });
 
 let postsArray = [];
